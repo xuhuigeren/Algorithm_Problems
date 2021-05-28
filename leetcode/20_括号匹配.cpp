ee63ec7be80bb5c902a6b25256532f0fb8070b16
henry <932374082@qq.com>
2021-05-28 17:31:11 +0800

    add 20_括号匹配

---
+++ leetcode/20_括号匹配.cpp
@@ -0,0 +1,68 @@
+/*
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+ 
+示例
+
+输入：s = "()"
+输出：true
+
+输入：s = "()[]{}"
+输出：true
+
+输入：s = "(]"
+输出：false
+
+输入：s = "([)]"
+输出：false
+
+输入：s = "{[]}"
+输出：true
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        unordered_map<char, char> pairs = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        stack<char> stk;
+        for (char ch: s) {
+            if (pairs.count(ch)) { //左括号为0，压入栈
+                if (stk.empty() || stk.top() != pairs[ch]) {
+                    return false;
+                }
+                stk.pop();
+            }
+            else {
+                stk.push(ch);
+            }
+        }
+        return stk.empty();
+    }
+};
+
+int main(){
+
+    string s =  "()[]{}";
+    bool res = Solution().isValid(s);
+    cout << res <<endl;
+    return 0;
+    
+}
+
