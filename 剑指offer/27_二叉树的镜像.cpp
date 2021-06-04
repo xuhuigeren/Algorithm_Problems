e5054c98570fbc1a7f8d417282de412624d396c9
henry <932374082@qq.com>
2021-06-05 07:21:47 +0800

    add 27_二叉树的镜像

---
+++ 剑指offer/27_二叉树的镜像.cpp
@@ -0,0 +1,37 @@
+/*
+请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+
+例如输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+镜像输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+ 
+
+示例 1：
+
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+
+*/
+//递归法
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) return root;
+            swap(root->left, root->right); // 中
+            invertTree(root->left); // 左
+            invertTree(root->right); // 右
+            return root;
+}
+};
