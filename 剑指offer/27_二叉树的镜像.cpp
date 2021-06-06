4e32d4f24416ca4d6e25454c399b9258dc1b3f92
henry <932374082@qq.com>
2021-06-06 15:10:35 +0800

    modified

--- 剑指offer/27_二叉树的镜像.cpp
+++ 剑指offer/27_二叉树的镜像.cpp
@@ -29,9 +29,9 @@ class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
         if (root == NULL) return root;
-            swap(root->left, root->right); // 中
-            invertTree(root->left); // 左
-            invertTree(root->right); // 右
-            return root;
+        swap(root->left, root->right); // 中
+        invertTree(root->left); // 左
+        invertTree(root->right); // 右
+        return root;
 }
 };
