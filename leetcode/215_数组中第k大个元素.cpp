be090c1b9a5aa541a61fdc474d878c4efef57315
henry <932374082@qq.com>
2021-06-09 10:37:29 +0800

    modified 215_数组中第k大个元素

--- leetcode/215_数组中第k大个元素.cpp
+++ leetcode/215_数组中第k大个元素.cpp
@@ -50,36 +50,61 @@ public:
 };
 
 
+
+//快速排序！！
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
+
+    int partition(vector<int> &nums, int left, int right){
+        int i=left, j=right;
+        //随机选择
+        int rd = rand()%(right-left+1)+left;
+        swap(nums[rd], nums[right]);
+
+        while(i<j){
+            while(i<j && nums[i]>=nums[right]) i++;
+            while(i<j && nums[j]<=nums[right]) j--;
+            swap(nums[i], nums[j]);
+        }
+        swap(nums[i],nums[right]);
+        return i;
+        
+    }
 public:
-    int quickSelect(vector<int>& a, int l, int r, int index) {
-        int q = randomPartition(a, l, r);
-        if (q == index) {
-            return a[q];
-        } else {
-            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);
-        }
-    }
-
-    inline int randomPartition(vector<int>& a, int l, int r) {
-        int i = rand() % (r - l + 1) + l;
-        swap(a[i], a[r]);
-        return partition(a, l, r);
-    }
-
-    inline int partition(vector<int>& a, int l, int r) {
-        int x = a[r], i = l - 1;
-        for (int j = l; j < r; ++j) {
-            if (a[j] <= x) {
-                swap(a[++i], a[j]);
-            }
-        }
-        swap(a[i + 1], a[r]);
-        return i + 1;
-    }
-
     int findKthLargest(vector<int>& nums, int k) {
-        srand(time(0));
-        return quickSelect(nums, 0, nums.size() - 1, nums.size() - k);
+        int left=0, right=nums.size()-1;       
+        while (true)
+        {
+            int t = partition(nums,left,right);
+            if(t==k-1)  return nums[t];
+            if(t<k-1)   left=t+1;
+            if(t>k-1)   right=t-1;
+        }      
+        return nums[k-1];
+       
     }
 };
+
+int main(){
+
+    vector<int> arr;
+    arr.push_back(2);
+    arr.push_back(7);
+    arr.push_back(4);
+    arr.push_back(6);
+    arr.push_back(5);
+    arr.push_back(1);
+    arr.push_back(3);
+    arr.push_back(8);
+    arr.push_back(9);
+    // arr.push_back(99);
+    // arr.push_back(99);
+    // arr.push_back(99);
+
+    int k = 8 ;
+    int res = Solution().findKthLargest(arr,k);
+    cout<< res<<endl;
+
+}
