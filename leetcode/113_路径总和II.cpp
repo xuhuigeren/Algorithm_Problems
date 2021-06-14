5a3ad95d1809cf8638e2b116360cdec17a71aa31
henry <932374082@qq.com>
2021-06-14 10:46:40 +0800

    modified 113_路径总和II

--- leetcode/113_路径总和II.cpp
+++ leetcode/113_路径总和II.cpp
@@ -39,3 +39,43 @@
 
 
 */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //全局变量
+    vector<vector <int>> res;
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<int> path;
+        dfs(root,targetSum,path);
+        return res;
+
+    }
+
+    void dfs(TreeNode* root, int sum, vector<int> path){
+        if(!root)  return;
+        sum=sum-root->val;
+        path.push_back(root->val);
+        //递归终止条件
+        if(!root->left&&!root->right&&sum==0){
+            res.push_back(path);
+            return;
+        }
+        //递归
+        dfs(root->left,sum,path);
+        dfs(root->right,sum,path);
+
+    }
+};
