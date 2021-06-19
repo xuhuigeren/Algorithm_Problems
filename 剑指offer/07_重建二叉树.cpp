65fb9357932dde7e9d5f7a5154c927985bbe2872
henry <932374082@qq.com>
2021-06-19 18:50:39 +0800

    modified 07_重建二叉树

--- 剑指offer/07_重建二叉树.cpp
+++ 剑指offer/07_重建二叉树.cpp
@@ -63,3 +63,43 @@ public:
 
     }
 };
+
+
+
+class Solution {
+private:
+    unordered_map<int, int> mp;
+
+public:
+    TreeNode* myBuildTree(const vector<int>& preorder, const vector<int>& inorder, int preleft, int preright, int inleft, int inright) {
+        if (preorder_left > preorder_right) {
+            return nullptr;
+        }
+        
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preleft;
+        // 在中序遍历中定位根节点
+        int inorder_root = mp[preorder[preorder_root]];
+        
+        // 先把根节点建立出来
+        TreeNode* root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inleft;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root->left = myBuildTree(preorder, inorder, preleft + 1, preleft + size_left_subtree, inleft, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root->right = myBuildTree(preorder, inorder, preleft + size_left_subtree + 1, preright, inorder_root + 1, inright);
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        // 构造哈希映射，帮助我们快速定位根节点
+        for (int i = 0; i < n; ++i) {
+            mp[inorder[i]] = i;
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+};
